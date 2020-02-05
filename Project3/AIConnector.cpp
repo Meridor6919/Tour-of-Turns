7c4dad7bfc59fd63aa6fe607a2987fa783439fad
Michał Balicki <michalbalicki@o2.pl>
2020-02-05 23:06:39 +0100

    Refactored AIConnector

--- Project3/AIConnector.cpp
+++ Project3/AIConnector.cpp
@@ -1,34 +1,68 @@
 #include "AIConnector.h"
 
-AIConnector::AIConnector(std::string path)
+AIConnector::AIConnector(std::string connector_path, int buffer_size)
 {
-	this->path = path;
+	this->connector_path = connector_path;
+	this->buffer_size = buffer_size;
 }
-void AIConnector::Write(std::string msg)
+bool AIConnector::Write(std::string msg)
 {
 	unsigned long bytes_written;
-	msg += "\n";
-	WriteFile(input_pipe_write, msg.c_str(), static_cast<DWORD>(msg.size()), &bytes_written, NULL);
+	char *buffer = new char[buffer_size];
+	strcpy_s(buffer, buffer_size, (msg+'\n').c_str());
+	const bool result = WriteFile(input_pipe_write, buffer, buffer_size, &bytes_written, NULL);
+	if (result)
+	{
+		MessageBox(0, std::to_string(GetLastError()).c_str(), "Pipe Error", 0);
+	}
+	delete [] buffer;
+	return result;
 }
-std::string AIConnector::Read()
+bool AIConnector::Read(std::string &msg_received)
 {
-	std::string ret = "";
 	unsigned long bytes_read;
-	char buffer[2048] = "";
+	char *buffer = new char[buffer_size];
+	const bool result = ReadFile(output_pipe_read, buffer, buffer_size, &bytes_read, NULL);
 
-	ReadFile(output_pipe_read, buffer, sizeof(buffer) - 1, &bytes_read, NULL);
-	std::cout << buffer;
-	return ret;
+	if (result)
+	{
+		msg_received = buffer;
+		msg_received.erase(msg_received.begin() + msg_received.find('\r'), msg_received.end());
+	}
+	else
+	{
+		MessageBox(0, std::to_string(GetLastError()).c_str(), "Pipe Error", 0);
+	}
+	delete[] buffer;
+	return result;
 }
 AIConnector::~AIConnector()
 {
 	Write(exit_command);
 	handling_connection = false;
-	recv_thread->join();
-	if (input_pipe_write != NULL) CloseHandle(input_pipe_write);
-	if (input_pipe_read != NULL) CloseHandle(input_pipe_read);
-	if (output_pipe_write != NULL) CloseHandle(output_pipe_write);
-	if (output_pipe_read != NULL) CloseHandle(output_pipe_read);
-	if (process_info.hProcess != NULL) CloseHandle(process_info.hProcess);
-	if (process_info.hThread != NULL) CloseHandle(process_info.hThread);
+	connection_thread->join();
+	if (input_pipe_write != NULL)
+	{
+		CloseHandle(input_pipe_write);
+	}
+	if (input_pipe_read != NULL)
+	{
+		CloseHandle(input_pipe_read);
+	}
+	if (output_pipe_write != NULL) 
+	{
+		CloseHandle(output_pipe_write);
+	}
+	if (output_pipe_read != NULL) 
+	{
+		CloseHandle(output_pipe_read);
+	}
+	if (process_info.hProcess != NULL)
+	{
+		CloseHandle(process_info.hProcess);
+	}
+	if (process_info.hThread != NULL)
+	{
+		CloseHandle(process_info.hThread);
+	}
 }
--- Project3/AIConnector.h
+++ Project3/AIConnector.h
@@ -4,10 +4,10 @@
 #include <iostream>
 #include <string>
 #include <thread>
-#include "Constants.h"
 
 class AIConnector
 {
+	int buffer_size;
 	const std::string start_command = "start";
 	const std::string exit_command = "exit";
 	PROCESS_INFORMATION process_info = { 0 };
@@ -15,8 +15,8 @@ class AIConnector
 	HANDLE input_pipe_read;
 	HANDLE output_pipe_write;
 	HANDLE output_pipe_read;
-	std::string path;
-	std::unique_ptr<std::thread> recv_thread;
+	std::string connector_path;
+	std::unique_ptr<std::thread> connection_thread;
 	bool handling_connection;
 
 	template <class T>
@@ -26,9 +26,9 @@ public:
 
 	template <class T>
 	bool HandleConnection(void(T::*MsgHandling)(std::string), T* object);
-	AIConnector(std::string path);
-	void Write(std::string msg);
-	std::string Read();
+	AIConnector(std::string connector_path, int buffer_size);
+	bool Write(std::string msg_send);
+	bool Read(std::string &msg_received);
 	~AIConnector();
 };
 
@@ -38,37 +38,49 @@ bool AIConnector::HandleConnection(void(T::*MsgHandling)(std::string), T* object
 	SECURITY_ATTRIBUTES security_atributes = { 0 };
 	security_atributes.bInheritHandle = 1;
 
-	CreatePipe(&output_pipe_read, &output_pipe_write, &security_atributes, 0);
-	CreatePipe(&input_pipe_read, &input_pipe_write, &security_atributes, 0);
+	if (!CreatePipe(&output_pipe_read, &output_pipe_write, &security_atributes, 0))
+	{
+		MessageBox(0, std::to_string(GetLastError()).c_str(), "Pipe Error", 0);
+		return false;
+	}
+	if(!CreatePipe(&input_pipe_read, &input_pipe_write, &security_atributes, 0))
+	{
+		MessageBox(0, std::to_string(GetLastError()).c_str(), "Pipe Error", 0);
+		return false;
+	}
 
 	STARTUPINFO startup_info = { 0 };
 	startup_info.dwFlags = STARTF_USESHOWWINDOW | STARTF_USESTDHANDLES;
 	startup_info.hStdInput = input_pipe_read;
 	startup_info.hStdOutput = output_pipe_write;
 
-	CreateProcessA(NULL, (LPSTR)path.c_str(), NULL, NULL, TRUE, 0, NULL, NULL, &startup_info, &process_info);
-	Write(start_command);
+	if (!CreateProcessA(NULL, (LPSTR)connector_path.c_str(), NULL, NULL, TRUE, 0, NULL, NULL, &startup_info, &process_info))
+	{
+		MessageBox(0, std::to_string(GetLastError()).c_str(), "Pipe Error", 0);
+		return false;
+	}
+	if(!Write(start_command))
+	{
+		MessageBox(0, std::to_string(GetLastError()).c_str(), "Pipe Error", 0);
+		return false;
+	}
 	handling_connection = true;
-	recv_thread = std::make_unique<std::thread>(&AIConnector::RecvFunction<T>, this, MsgHandling, object);
+	connection_thread = std::make_unique<std::thread>(&AIConnector::RecvFunction<T>, this, MsgHandling, object);
 	return true;
 }
 template <class T>
 void  AIConnector::RecvFunction(void(T::*MsgHandling)(std::string), T* object)
 {
-	unsigned long bytes_read;
-
-
 	while (handling_connection)
 	{
-		char buffer[2048] = "";
-		if (!ReadFile(output_pipe_read, buffer, sizeof(buffer) - 1, &bytes_read, NULL))
+		std::string msg_received = "";
+		if (!Read(msg_received))
 		{
-			MessageBox(0, ErrorMsg::pipe_error.c_str(), ErrorTitle::pipe_error.c_str(), 0);
 			break;
 		}
 		else
 		{
-			std::invoke(MsgHandling, object, buffer);
+			std::invoke(MsgHandling, object, msg_received);
 		}
 
 	}
