24fc43302df3809ef8911a8591a8f95a0014bafa
Meridor <e.mail2000debian@gmail.com>
2021-10-03 22:21:16 +0200

    Refactored WavTransformer

--- Project3/MeridorLib/Misc/WavTransformer.h
+++ Project3/MeridorLib/Misc/WavTransformer.h
@@ -5,21 +5,23 @@
 #include <string>
 
 #pragma comment(lib, "winmm.lib")
-
-class WavTransformer
+namespace MeridorWavTransformer
 {
-	unsigned long file_size = 0;
-	byte* bytes;
-	float current_volume = 1.0f;
-	DWORD flags = NULL;
+	class WavTransformer
+	{
+		unsigned long file_size = 0;
+		byte* bytes = nullptr;
+		float current_volume = 1.0f;
+		DWORD flags = NULL;
 
-	void LoadBytes(const std::string& path);
-	unsigned long GetDataStartingIndex();
+		void LoadBytes(const std::string& path);
+		unsigned long GetDataStartingIndex();
 
-public:
-	bool Init(std::string path);
-	void SetFlags(DWORD flags);
-	void StartPlaying(float volume);
-	void StopPlaying();
-	~WavTransformer();
-};
+	public:
+		bool Init(std::string path);
+		void SetFlags(DWORD flags);
+		void StartPlaying(float volume);
+		void StopPlaying();
+		~WavTransformer();
+	};
+}
