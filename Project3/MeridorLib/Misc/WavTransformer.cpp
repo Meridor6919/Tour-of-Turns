e88b4f4d6fbbfb21336baa1140f75f0644a4b67c
Meridor <e.mail2000debian@gmail.com>
2021-10-10 16:31:07 +0200

    Refactored WavTransformer

--- Project3/MeridorLib/Misc/WavTransformer.cpp
+++ Project3/MeridorLib/Misc/WavTransformer.cpp
@@ -1,77 +1,148 @@
 #include "WavTransformer.h"
 namespace MeridorWavTransformer
 {
-	void WavTransformer::LoadBytes(const std::string& path)
+	long long WavTransformer::FindSound(const char* name) const noexcept
 	{
-		std::ifstream file(path.c_str(), std::ios::binary);
-
-		for (unsigned long i = 0; i < file_size; ++i)
+		for (int i = 0; i < sounds.size(); ++i)
 		{
-			bytes[i] = file.get();
+			if (sounds[i].name == name)
+			{
+				return i;
+			}
+		}
+		return -1;
+	}
+	void WavTransformer::LoadBytes(const char* path, size_t index) noexcept
+	{
+		std::ifstream file(path, std::ios::binary);
+
+		for (unsigned long i = 0; i < sounds[index].size; ++i)
+		{
+			sounds[index].bytes[i] = file.get();
 		}
 		file.close();
 	}
-	unsigned long WavTransformer::GetDataStartingIndex()
+	bool WavTransformer::Erase(size_t index) noexcept
 	{
-		unsigned long  index = 0;
-
-		for (; index < file_size; ++index)
+		if (index < sounds.size())
 		{
-			if (bytes[index] == 'd' && bytes[index + 1] == 'a' && bytes[index + 2] == 't' && bytes[index + 3] == 'a')
+			delete[] sounds[index].bytes;
+			sounds.erase(sounds.begin() + index);
+			return true;
+		}
+		return false;
+	}
+	bool WavTransformer::Erase(const char* name) noexcept
+	{
+		return Erase(FindSound(name));
+	}
+	bool WavTransformer::Add(const char* path) noexcept
+	{
+		bool already_loaded = FindSound(path) > 0;
+
+		if (!already_loaded)
+		{
+			std::ifstream file(path, std::ios::binary | std::ios::ate);
+			size_t file_size = static_cast<size_t>(file.tellg());
+			sounds.emplace_back(new byte[file_size], path, file_size);
+
+			bool valid_file = file.good();
+			file.close();
+
+			if (valid_file)
 			{
-				return index + 8;
+				LoadBytes(path, sounds.size() - 1);
+			}
+			return valid_file;
+		}
+		return true;
+	}
+	unsigned long WavTransformer::GetDataStartingIndex(size_t index) const noexcept
+	{
+		unsigned long  i = 0;
+
+		for (; i < sounds[index].size; ++i)
+		{
+			for (int j = 0; j < strlen(Internal::unique_identifier); ++j)
+			{
+				if (sounds[index].bytes[i] != Internal::unique_identifier[i])
+				{
+					break;
+				}
+				if (j == Internal::unique_identifier_length)
+				{
+					return i + Internal::data_offset;
+				}
 			}
 		}
 		return 0;
 	}
-	bool WavTransformer::Init(std::string path)
-	{
-		std::ifstream file(path.c_str(), std::ios::binary | std::ios::ate);
-		file_size = static_cast<unsigned long>(file.tellg());
-		bytes = new byte[file_size];
-		bool valid_file = file.good();
-		file.close();
 
-		if (valid_file)
-		{
-			LoadBytes(path);
-		}
-		return valid_file;
-	}
-	void WavTransformer::SetFlags(DWORD flags)
+	void WavTransformer::SetFlags(DWORD flags) noexcept
 	{
 		this->flags = flags;
 	}
-	void WavTransformer::StartPlaying(float volume)
+	void WavTransformer::StartPlaying(float volume) noexcept
 	{
-		if (!volume)
+		if (not_muted && sounds.size() > 0)
 		{
-			StopPlaying();
+			PlaySoundA(reinterpret_cast<LPCSTR>(sounds[current_index].bytes), 0, SND_MEMORY | flags);
 		}
 		else
 		{
-			//Data is stored in 2-byte long intervals and because of that short type is used
-			//Multiplying those values by numbers from range 0.0 - 1.0 reduces volume by according percentage
-			//There is built-in inaccuracy, because of floating point multiplication with short type
-			volume /= current_volume;
-			unsigned long index = GetDataStartingIndex();
-			short* widened_byte_ptr = reinterpret_cast<short*>(&bytes[index]);
-			for (; index < file_size; index += 2)
-			{
-				*widened_byte_ptr = static_cast<short>((*widened_byte_ptr) * volume);
-				++widened_byte_ptr;
-			}
-			current_volume *= volume;
-			PlaySoundA(reinterpret_cast<LPCSTR>(bytes), 0, SND_MEMORY | flags);
+			StopPlaying();
 		}
 	}
-	void WavTransformer::StopPlaying()
+	bool WavTransformer::ChangeSound(size_t index) noexcept
+	{
+		if (index < sounds.size())
+		{
+			current_index = index;
+			return true;
+		}
+		return false;
+	}
+	bool WavTransformer::ChangeSound(const char* name) noexcept
+	{
+		return ChangeSound(FindSound(name));
+	}
+	void WavTransformer::StopPlaying() noexcept
 	{
 		PlaySoundA(0, 0, 0);
 	}
 	WavTransformer::~WavTransformer()
 	{
 		StopPlaying();
-		delete[file_size] bytes;
+		for (long long i = static_cast<long long>(sounds.size()) - 1; i >= 0; --i)
+		{
+			Erase(i);
+		}
+	}
+	void WavTransformer::ChangeVolume(float volume) noexcept
+	{
+		if (volume)
+		{
+			//Data is stored in 2-byte long intervals and because of that short type is used
+			//Multiplying those values by numbers from range 0.0 - 1.0 reduces volume by according percentage
+			//There is built-in inaccuracy, because of floating point multiplication with short type
+			not_muted = false;
+			volume /= sounds[current_index].volume;
+			unsigned long index = GetDataStartingIndex(current_index);
+			short* widened_byte_ptr = reinterpret_cast<short*>(&sounds[current_index].bytes[index]);
+			for (; index < sounds[current_index].size; index += 2)
+			{
+				*widened_byte_ptr = static_cast<short>((*widened_byte_ptr) * volume);
+				++widened_byte_ptr;
+			}
+			sounds[current_index].volume *= volume;
+		}
+		else
+		{
+			not_muted = false;
+		}
+	}
+	float WavTransformer::GetVolume() const noexcept
+	{
+		return not_muted * sounds[current_index].volume;
 	}
 }
